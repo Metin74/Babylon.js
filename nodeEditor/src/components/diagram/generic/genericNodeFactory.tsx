1a68e189a0ac914bb326ce2ab8ceb5bbf7533aaa
David Catuhe <david.catuhe@live.fr>
2019-08-13 08:07:51 -0700

    .

--- nodeEditor/src/components/diagram/generic/genericNodeFactory.tsx
+++ nodeEditor/src/components/diagram/generic/genericNodeFactory.tsx
@@ -3,7 +3,7 @@ import { GenericNodeModel } from "./genericNodeModel";
 import * as React from "react";
 import { GlobalState } from '../../../globalState';
 import { AbstractReactFactory, GenerateWidgetEvent } from '@projectstorm/react-canvas-core';
-import { DiagramEngine } from '@projectstorm/react-diagrams';
+import { DiagramEngine } from '@projectstorm/react-diagrams-core';
 
 export class GenericNodeFactory extends AbstractReactFactory<GenericNodeModel, DiagramEngine> {
     private _globalState: GlobalState;
